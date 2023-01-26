f65ea90ae5a3361b53185903fb41d112e7a74ddc
taku-kw <riverbooktable@gmail.com>
2023-01-26 19:14:39 +0900

    Implement onUpdate

--- app/src/main/java/com/example/widget_sample/WidgetSampleWidgetProvider.kt
+++ app/src/main/java/com/example/widget_sample/WidgetSampleWidgetProvider.kt
@@ -1,6 +1,34 @@
 package com.example.widget_sample
 
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
 
 class WidgetSampleWidgetProvider : AppWidgetProvider() {
+
+    private val text = "Hello Widget World!"
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+            .let { intent ->
+                PendingIntent.getActivity(context, 0, intent, 0)
+            }
+
+        appWidgetIds?.forEach {id ->
+            val views: RemoteViews = RemoteViews(
+                context?.packageName,
+                R.layout.widget_sample_widget
+            ).apply {
+                setOnClickPendingIntent(R.id.textView, pendingIntent)
+            }
+
+            appWidgetManager?.updateAppWidget(id, views)
+        }
+    }
 }
